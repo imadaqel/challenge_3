45868952d6e4a022d27cc3d9c0cbdd3ad00a4ffe
imadaqel <emadaqel92@gmail.com>
2020-11-04 14:06:46 +0200

    Yeah

--- server.js
+++ server.js
@@ -1,2 +1,22 @@
 
+var express = require('express');
+//app.use(express.static('client');
+var app = express();
 
+
+app.use(express.json());
+app.use(express.urlencoded());
+// app.post('/',function(req,res){
+//     // make JSON strengify fr Objec
+//    
+//  console.log(response,req.body);  
+
+
+//  });
+app.get('/', (req, res) => {
+    //res.send('Hello World!')
+    res.render("index")
+})
+app.listen(3000, function () {
+    console.log("server at localhost:3000");
+})
